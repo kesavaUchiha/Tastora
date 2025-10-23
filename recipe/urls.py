a75a9e56de6b4885857d415a556c511e19fa5006
kesav <kesava@testpress.com>
2025-10-23 18:39:45 +0530

    info: Initial Recipe Setup

--- recipe/urls.py
+++ recipe/urls.py
@@ -3,5 +3,5 @@ from . import views
 app_name='recipe'
 urlpatterns=[
     path('home/',views.HomePage.as_view(),name='home'),
-    path('recipe//',views.RecipePage.as_view(),name='recipe')
+    path('recipes/',views.RecipePage.as_view(),name='recipes')
 ]
